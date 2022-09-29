cb872f2bb987a7039d0c25e310a3417164e10429
ASFahim007 <devasfahim@gmail.com>
2022-09-29 14:35:54 +0600

    user profile is done

--- badminton-pro/src/components/Information/Information.js
+++ badminton-pro/src/components/Information/Information.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Information.css'
+import userImg from '../../image/my-image.jpg'
 
 const Information = (props) => {
     const [breakTime, setBreakTime] = useState(0);
@@ -8,12 +9,28 @@ const Information = (props) => {
     }
     return (
         <div className='information'>
-            <h5>Add a Break</h5>
-            <div className="break_time">
-                <span onClick={()=> addBreakTime(10)}>10m</span>
-                <span onClick={()=> addBreakTime(20)}>20m</span>
-                <span onClick={()=> addBreakTime(40)}>40m</span>
-                <span onClick={()=> addBreakTime(60)}>60m</span>
+            <div className="user_info">
+                <div className="user">
+                    <img src={userImg} alt="" />
+                    <div>
+                        <h5>A S Fahim</h5>
+                        <small className='text-muted'>Badminton player</small>
+                    </div>
+                </div>
+                <div className="profile">
+                    <p><strong>Olympic:</strong> <strong className='text-warning'>2 Gold</strong></p>
+                    <p><strong>World cup:</strong> <strong className='text-warning'>3 Gold</strong></p>
+                    <p><strong>Sudirman cup:</strong> <strong className='text-muted'>2 Silver</strong></p>
+                </div>
+            </div>
+            <div>
+                <h5>Add a Break</h5>
+                <div className="break_time">
+                    <span onClick={()=> addBreakTime(10)}>10m</span>
+                    <span onClick={()=> addBreakTime(20)}>20m</span>
+                    <span onClick={()=> addBreakTime(40)}>40m</span>
+                    <span onClick={()=> addBreakTime(60)}>60m</span>
+                </div>
             </div>
             <div className="exercise_time">
                 <h5 className=''>Exercise Details</h5>
